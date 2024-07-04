fa3745153b38f6714c200cafe9dca7894c4c4cdd
Peter Reutemann <fracpete@gmail.com>
2024-07-05 11:28:36 +1200

    from-docx-pt now uses *.docx as default glob; prepared for release

--- src/ldc_docx/pretrain/_docx.py
+++ src/ldc_docx/pretrain/_docx.py
@@ -85,7 +85,7 @@ class DOCXPretrainReader(PretrainReader):
         Initializes the reading, e.g., for opening files or databases.
         """
         super().initialize()
-        self._inputs = locate_files(self.source, input_lists=self.source_list, fail_if_empty=True)
+        self._inputs = locate_files(self.source, input_lists=self.source_list, fail_if_empty=True, default_glob="*.docx")
         self._current_doc = None
 
     def read(self) -> Iterable[PretrainData]:
